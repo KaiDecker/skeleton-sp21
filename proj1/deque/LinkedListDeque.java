dafecd16991beae20e51626130d14db8abdce2b9
咸蛋超人迪卡 <2280203395@qq.com>
2025-11-04 14:13:46 +0800

    Complete Proj1 part1

---
+++ proj1/deque/LinkedListDeque.java
@@ -0,0 +1,151 @@
+package deque;
+
+import java.util.Iterator;
+
+public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
+    private int size;
+    private final Node sentinel;
+
+    private class Node {
+        private Node prev;
+        private Node next;
+        private T elem;
+    }
+
+    public LinkedListDeque() {
+        this.sentinel = new Node();
+        // 哨兵的下一个和上一个都指向自己
+        this.sentinel.next = this.sentinel;
+        this.sentinel.prev = this.sentinel;
+        this.sentinel.elem = null;
+        this.size = 0;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        Node p = new Node();
+        p.elem = item;
+        p.next = sentinel.next;
+        p.prev = sentinel;
+        p.next.prev = p;
+        sentinel.next = p;
+        size++;
+    }
+    @Override
+    public void addLast(T item) {
+        Node p = new Node();
+        p.elem = item;
+        p.next = sentinel;
+        p.prev = sentinel.prev;
+        p.prev.next = p;
+        sentinel.prev = p;
+        size++;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        Node p = sentinel.next;
+        while (p.next != sentinel) {
+            System.out.print(p.elem + " ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    @Override
+    public T removeFirst() {
+        if (sentinel.next == sentinel) {
+            return null;
+        }
+        Node p = sentinel.next;
+        sentinel.next = p.next;
+        p.next.prev = sentinel;
+        size -= 1;
+        return p.elem;
+    }
+
+    @Override
+    public T removeLast() {
+        if (sentinel.prev == sentinel) {
+            return null;
+        }
+        Node p = sentinel.prev;
+        sentinel.prev = p.prev;
+        p.prev.next = sentinel;
+        size -= 1;
+        return p.elem;
+    }
+
+    @Override
+    public T get(int index) {
+        Node p = sentinel.next;
+        while (index != 0 && p != sentinel) {
+            p = p.next;
+            index -= 1;
+        }
+        if (index == 0) {
+            return p.elem;
+        }
+        return null;
+    }
+
+    public T getRecursive(int index) {
+        Node p = sentinel.next;
+        if (index != 0 && p == sentinel) {
+            return null;
+        }
+        if (index == 0) {
+            return p.elem;
+        }
+        LinkedListDeque<T> l = new LinkedListDeque<>();
+        l.sentinel.next = p.next;
+        return l.getRecursive(index - 1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // 检查 o 是否是 Deque 实例，或者队列的大小是否相同
+        if (!(o instanceof Deque) || ((Deque<?>) o).size() != this.size()) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+        // 逐个元素比较
+        for (int i = 0; i < this.size(); i++) {
+            Object item = ((Deque<?>) o).get(i);
+            if (!(this.get(i).equals(item))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Iterator<T> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator<T> {
+        private Node node;
+
+        LinkedListIterator() {
+            this.node = sentinel.next;
+        }
+
+        public boolean hasNext() {
+            return this.node != sentinel;
+        }
+
+        public T next() {
+            T ret = this.node.elem;
+            this.node = this.node.next;
+            return ret;
+        }
+
+    }
+}
---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,34 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
+    private final Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        this.comparator = c;
+    }
+
+    public T max() {
+        return getMax(this.comparator);
+    }
+
+    public T max(Comparator<T> c) {
+        return getMax(c);
+    }
+
+    private T getMax(Comparator<T> c) {
+        if (isEmpty()) {
+            return null;
+        }
+        T m = this.get(0);
+        for (int i = 1; i < this.size(); i++) {
+            T temp = this.get(i);
+            if (c.compare(m, temp) < 0) {
+                m = temp;
+            }
+        }
+        return m;
+    }
+}
