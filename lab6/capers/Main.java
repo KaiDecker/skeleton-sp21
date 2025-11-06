7dae66d117cad57d4dc55697063f4839c6af89ac
咸蛋超人迪卡 <2280203395@qq.com>
2025-11-06 14:35:06 +0800

    Fixed

--- lab6/capers/Main.java
+++ lab6/capers/Main.java
@@ -58,8 +58,8 @@ public class Main {
             CapersRepository.makeDog(name, breed, age);
         case "birthday":
             validateNumArgs("birthday", args, 2);
-            String name = args[1];
-            CapersRepository.celebrateBirthday(name);
+            String Name = args[1];
+            CapersRepository.celebrateBirthday(Name);
         default:
             exitWithError(String.format("Unknown command: %s", args[0]));
         }
